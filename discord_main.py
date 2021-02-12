1d2085233f7f94f70b0e81d693f130bd11153e9c
chibi-chichi <78858443+chibi-chichi@users.noreply.github.com>
2021-02-12 23:31:06 +0900

    add discord bot

---
+++ discord_main.py
@@ -0,0 +1,25 @@
+import discord
+from discord.ext import commands
+import logging
+
+TOKEN = "NzAxNzk1NTMzOTkxMTgyMzkw.Xp2sBg.t5pM0yybYfPcbkVxnH6bVc9dC6g"
+
+bot = commands.Bot(command_prefix='-')
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('{0}준비 완료!'.format(self.user))
+
+    async def on_message(self, message):
+        print("{0.author}에게서 온 DM : {0.content}".format(message))
+
+    async def on_message(self, message):
+        if message.author == client.user:
+            return
+
+        if message.content == '안녕':
+            await message.channel.send("안녕하세요!!")
+
+logging.basicConfig(level=logging.INFO)
+client = MyClient()
+client.run(TOKEN)
