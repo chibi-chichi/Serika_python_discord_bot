ac680468d023375443fa4451115a406e791a379e
chibi-chichi <78858443+chibi-chichi@users.noreply.github.com>
2021-02-13 23:52:07 +0900

    Update discord_main.py

--- discord_main.py
+++ discord_main.py
@@ -5,7 +5,6 @@ import logging
 TOKEN = process.env.TOKEN
 
 bot = commands.Bot(command_prefix='-')
-
 class MyClient(discord.Client):
     async def on_ready(self):
         print('{0}준비 완료!'.format(self.user))
@@ -13,12 +12,17 @@ class MyClient(discord.Client):
     async def on_message(self, message):
         print("{0.author}에게서 온 DM : {0.content}".format(message))
 
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
+@bot.command(pass_context=True)
+async def on_message(ctx):
+    if ctx.content == '안녕':
+        await ctx.channel.send("안녕하세요!")
 
-        if message.content == '안녕':
-            await message.channel.send("안녕하세요!")
+@bot.command(pass_context=True)
+async def army_date(ctx):
+    if ctx.content == '전역일':
+        print(army.remain_days())
+        print(army.remain_days_percent())
+        await ctx.channel.send("치비님의 남은 전역일 수는" + army.remain_days() + "일이며 현재" + army.remain_days_percent() + "%만큼 했습니다!")
 
 logging.basicConfig(level=logging.INFO)
 client = MyClient()
