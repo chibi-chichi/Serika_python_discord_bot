ec1c3ffbc3dbd08d4d560878c9cf73a68901eeff
chibi-chichi <78858443+chibi-chichi@users.noreply.github.com>
2021-02-14 16:40:40 +0900

    Update discord_main.py

--- discord_main.py
+++ discord_main.py
@@ -1,8 +1,10 @@
+import os
 import discord
-from discord.ext import commands
 import logging
+from discord.ext import commands
 import army_date_calculate as army
-TOKEN = process.env.TOKEN
+
+TOKEN = os.environ["TOKEN"]
 
 bot = commands.Bot(command_prefix='-')
 class MyClient(discord.Client):
