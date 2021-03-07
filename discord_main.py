5eea8b196cdbe7974891886396157b5867af8183
chibi-chichi <78858443+chibi-chichi@users.noreply.github.com>
2021-03-07 23:02:45 +0900

    Update discord_main.py

--- discord_main.py
+++ discord_main.py
@@ -23,20 +23,21 @@ class MyClient(discord.Client):
         if message.author.id == self.user.id:
             return
     
-    #인사를 건냅니다.
+    # 인사를 건냅니다.
         if message.content.startswith('-안녕'):
             await message.reply('안녕하세요!!', mention_author=True)
     
-    #진짜 수정한거 맞는 지 확인합니다. 고쳐졌다면서 안고쳐져있지만        
+    # 진짜 수정한거 맞는 지 확인합니다. 고쳐졌다면서 안고쳐져있지만        
         if message.content.startswith('-수정'):
             await message.reply('고쳐졌어요!!', mention_author=True)
     
-    #제작자의 남은 군생활을 알려줍니다.    
+    # 제작자의 남은 군생활을 알려줍니다.    
         if message.content.startswith('-복무일'):
             date = army.remain_days()
             date_percent = army.remain_days_percent()
             await message.channel.send("치비님의 남은 전역일 수는 " + date + "일이며 현재까지 " + date_percent + "%만큼 했습니다!")
-      
+    
+    # 로또 번호를 뽑아줍니다. 낙첨되도 책임은 지지 않습니다...  
         if message.content.startswith('-로또'):
             # 로또 번호 1~45까지의 숫자를 뽑아줍니다.
             possible_number_list = random.sample(range(1,45), 6)
@@ -45,13 +46,26 @@ class MyClient(discord.Client):
             # 로또 번호를 출력하기 위해 문자열로 바꾸고 출력하기 편하게 바꿔줍니다.
             number_list = ", ".join([str(numb) for numb in possible_number_list])
             await message.channel.send("이번 로또 추첨번호는 `" + number_list + "` 가 좋을 것 같아요!")
-
+    
+    # 0~10 중에서의 숫자를 뽑아줍니다. 
         if message.content.startswith('-추첨'):
             # 디스코드 이모지인 숫자들의 리스트
             possible_numb = [":zero:", ":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":keycap_ten:"]
             # 숫자들의 리스트에서 하나를 뽑아줍니다.
             picked = random.choice(possible_numb)
             await message.channel.send("숫자 " + picked + "가 나왔어요!")
+    
+    # 선택장애를 위해 선택을 대신 해줍니다.
+        if message.content.startswith('-선택'):
+            # 메세지를 불러옵니다
+            choice_msg = message.content
+            # .split()을 위해 "-선택" 이라는 단어를 제거합니다.
+            wanted_choice = choice_msg[3:]
+            # 선택지를 나눠 리스트화시킵니다.
+            select_choose = wanted_choice.split()
+            # 리스트화 된 선택지에서 하나를 골라줍니다.
+            choose = random.choice(select_choose)
+            await message.channel.send(choose + "가 좋을 것 같아요!")
 
 #기본적인 정보들을 로그에 출력해줍니다.
 logging.basicConfig(level=logging.INFO)
