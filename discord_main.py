a2a5345d23a994f5e3baa09b55acdd845880854c
chibi-chichi <78858443+chibi-chichi@users.noreply.github.com>
2021-04-17 23:31:57 +0900

    Update discord_main.py

--- discord_main.py
+++ discord_main.py
@@ -75,7 +75,7 @@ class MyClient(discord.Client):
             try:
                 scopes = ['https://spreadsheets.google.com/feeds']
                 json_creds = os.getenv("GOOGLE_KEYS")
-                creds_dict = json.loads(token)
+                creds_dict = json.loads(json_creds)
                 creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
                 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
                 
