ef70aea51b3944504a441b3a701f24c719e5dda8
chibi-chichi <78858443+chibi-chichi@users.noreply.github.com>
2021-04-21 22:55:43 +0900

    아무 것도 안넣었는데 일단 넣어봄

---
+++ MLTH_event_point_calculator.py
@@ -0,0 +1,10 @@
+def theater_point_calculation(current_score, object_score, items):
+    #공식이 뭐였지
+    print("시어터")
+
+def tour_point_calculation(current_score, object_score, items, progressivity):
+    #공식이 진짜 뭐였지
+    print("투어")
+
+if __name__ == '__main__':
+    main()
