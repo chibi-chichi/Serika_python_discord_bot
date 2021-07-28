9f78b5700a2f2a652a8d62ea66aaa1278da64019
1staward <as4290156@gmail.com>
2021-07-29 03:38:04 +0900

    1. name 'null' is not defined 오류 해결 2. 이모지 리액션 추가 (기능은 하지못함) 3. 이벤트 id, type 함수 추가

--- spread_sheet_reader.py
+++ spread_sheet_reader.py
@@ -1,11 +1,14 @@
 import os
+from json.tool import main
+
 from oauth2client.service_account import ServiceAccountCredentials
 import gspread
 import json
 import traceback
 import discord
 
-    # 구글 스프레드시트를 가져옵니다.
+
+# 구글 스프레드시트를 가져옵니다.
 def sync_spread():
     try:
         scopes = ['https://spreadsheets.google.com/feeds',
@@ -21,15 +24,16 @@ def sync_spread():
     except Exception as e:
         print(traceback.format_exc())
 
+
 def spread_information(game_title, game_song):
-    google_spread = sync_spread() # 구글 스프레드시트와 공유해서 출력하게 해줍니다.
-    gc1 = google_spread.open("리듬게임 스코어링 시트").worksheet(game_title) # 각 게임의 엑셀을 찾아가줍니다.
-    title = gc1.row_values(1) # 엑셀의 정렬한 순서를 나타냅니다.
-    game_song = int(game_song) + 1 # 1은 title이 출력이 되기 때문에 1부터 시작할려면 +1을 해줘야합니다.
-    #score_values = gc1.row_values(game_song)
-    #japan_name = score_values[1]
+    google_spread = sync_spread()  # 구글 스프레드시트와 공유해서 출력하게 해줍니다.
+    gc1 = google_spread.open("리듬게임 스코어링 시트").worksheet(game_title)  # 각 게임의 엑셀을 찾아가줍니다.
+    title = gc1.row_values(1)  # 엑셀의 정렬한 순서를 나타냅니다.
+    game_song = int(game_song) + 1  # 1은 title이 출력이 되기 때문에 1부터 시작할려면 +1을 해줘야합니다.
+    # score_values = gc1.row_values(game_song)
+    # japan_name = score_values[1]
     # 위에 있는 코드들은 엑셀이 다 채워진 후 최적화를 해줄겁니다.
-    
+
     # 엄청난 노가다의 빛으로 비어있는 엑셀칸까지 다 출력해줍니다.
     japan_name = str(gc1.acell('B' + str(game_song)).value)
     korean_name = str(gc1.acell('C' + str(game_song)).value)
@@ -45,7 +49,7 @@ def spread_information(game_title, game_song):
     max_combo = str(gc1.acell('M' + str(game_song)).value)
     full_combo = str(gc1.acell('N' + str(game_song)).value)
     best_score = str(gc1.acell('O' + str(game_song)).value)
-    
+
     # 노래 제목을 한 번에 출력시켜주는 변수를 생성했습니다.
     song_title = japan_name + "\t" + korean_name
 
@@ -53,26 +57,27 @@ def spread_information(game_title, game_song):
     if japan_name == korean_name:
         song_title = japan_name
     # 디스코드에서 출력되는 메세지를 embed로 정리했습니다. embed에 추후 앨범 커버 사진 추가 예정
-    embed = discord.Embed(title="베스트 스코어", description='자신이 가장 플레이 한 곡 중에서 제일 잘한 기록을 가져옵니다.', color = 0x9B59B6)
-    embed.add_field(name="__TITLE__", value= song_title, inline=True)
+    embed = discord.Embed(title="베스트 스코어", description='자신이 가장 플레이 한 곡 중에서 제일 잘한 기록을 가져옵니다.', color=0x9B59B6)
+    embed.add_field(name="__TITLE__", value=song_title, inline=True)
     embed.add_field(name="__" + title[3] + "__", value=song_type, inline=True)
     embed.add_field(name="__" + title[4] + "__", value=artist, inline=False)
     embed.add_field(name="__" + title[5] + "__", value=difficulty, inline=False)
-    #embed.add_field(name="__ADJUSTMENT__",
+    # embed.add_field(name="__ADJUSTMENT__",
     #                value="**" + title[6] + "**\t" + perfect_note + "\n**" + title[7] + "**\t" + great_note + "\n**" +
     #                      title[8] + "**\t" + good_note + "\n**" + title[9] + "**\t" + badfastslow_note + "\n**" +
     #                      title[10] + "**\t" + miss_note + "\n**" + title[11] + "**\t" + total_note + "\n**" + title[
     #                          12] + "**\t" + max_combo + "\n**" + title[13] + "**\t" + full_combo, inline=False)
-    embed.add_field(name=title[6], value = perfect_note, inline=True)
-    embed.add_field(name=title[7], value = great_note, inline=True)
-    embed.add_field(name=title[8], value = good_note, inline=True)
-    embed.add_field(name=title[9], value = badfastslow_note, inline=True)
-    embed.add_field(name=title[10], value = miss_note, inline=True)
-    embed.add_field(name=title[11], value = total_note, inline=True)
-    embed.add_field(name=title[12], value = max_combo, inline=True)
-    embed.add_field(name=title[13], value = full_combo, inline=True)
+    embed.add_field(name=title[6], value=perfect_note, inline=True)
+    embed.add_field(name=title[7], value=great_note, inline=True)
+    embed.add_field(name=title[8], value=good_note, inline=True)
+    embed.add_field(name=title[9], value=badfastslow_note, inline=True)
+    embed.add_field(name=title[10], value=miss_note, inline=True)
+    embed.add_field(name=title[11], value=total_note, inline=True)
+    embed.add_field(name=title[12], value=max_combo, inline=True)
+    embed.add_field(name=title[13], value=full_combo, inline=True)
     embed.add_field(name="__HIGH SCORE__", value=best_score, inline=False)
     return embed
+
 
 if __name__ == '__main__':
     main()
