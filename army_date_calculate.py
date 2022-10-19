b75b14799b3334fd13a977de313b98c2b582e133
chibi-chichi <78858443+chibi-chichi@users.noreply.github.com>
2022-10-19 13:57:11 -0700

    fixed army_date_calculate.py

--- army_date_calculate.py
+++ army_date_calculate.py
@@ -6,12 +6,8 @@ import time
 def remain_days():
     today = date.today()                    # 현재 시간을 가져옵니다. (YYYY-MM-DD)
     dead_end = datetime.date(2022, 2, 2)    # 축 전역일 (YYYY-MM-DD)
-
-
-    object = abs(today - dead_end)          # 남은 일수를 알려줍니다. (DDD Days)
-    object_days = object.days               # 남은 일자를 숫자로만 출력시켜줍니다.
-    object_day = str(object_days)
-    return object_day
+    day_left = str(abs(today - dead_end))          # 남은 일수를 알려줍니다. (DDD Days)
+    return day_left
     
     # 남은 군 생활을 퍼센테이지로 나타내줍니다.
 def remain_days_percent():
@@ -19,6 +15,3 @@ def remain_days_percent():
     object_days_percent = round(abs((object_days - 548) / 548 * 100), 2)    # 남은 일수의 퍼센테이지를 구합니다. 그 날이 올까?
     object_days_percent = str(object_days_percent)
     return object_days_percent
-
-if __name__ == '__main__':
-    main()
